5397f53b6858d33a95ba5ffb1d82cdea99a2e0bc
Jakub Razak <methodcz@gmail.com>
2015-12-02 16:54:49 +0800

    - Added readInputs, readHoldingRegisters, readInputRegisters  - Added options as optional input to pass function specific data. Ie unsigned option for reading registers. (reads as signed by default)  - Changed error codes evaluation logic slightly. Still to be worked on.

--- examples/test.js
+++ examples/test.js
@@ -1,6 +1,6 @@
 var ModbusTcp = require('../lib/modbustcp');
 
-var mtcp = new ModbusTcp('192.168.10.2', 502, {debug: true});
+var mtcp = new ModbusTcp('ifloodlights.eairlink.com', 502, {debug: true});
 
 mtcp.on('error', function(e){
     console.log(e);
@@ -9,14 +9,25 @@ mtcp.on('error', function(e){
 
 mtcp.connect(function(){
     // Test Read Coils
-    mtcp.readCoils(21, 13, function (err, data, request, response) {
+    mtcp.readCoils(0, 2010, function (err, data, request, response) {
         if(err) {
             console.log(err);
         } else {
             console.log(response);
         }
-        mtcp.disconnect(function(){
-            process.exit(0);
-        });
+    });
+    mtcp.readInputs(0, 5, function (err, data, request, response) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log(response);
+        }
+    });
+    mtcp.readHoldingRegisters(290, 126,function (err, data, request, response) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log(response);
+        }
     });
 });
