b31a25f44867551c71269acaacb32916925de918
Jakub Razak <methodcz@gmail.com>
2015-12-04 12:31:06 +0800

    - Added writeMultipleCoils and writeMultipleCoilsSameValue - Bugs fixing - Removed buffer slicing from buffer reader - Moved error codes to function codes as they require their own handler now as well

--- examples/test.js
+++ examples/test.js
@@ -15,28 +15,28 @@ mtcp.on('disconnect', function(){
 });
 
 mtcp.connect().then(function(){
-    //var promises = [];
-    //promises.push(mtcp.readCoils(0, 20));
-    //promises.push(mtcp.readDiscreteInputs(0, 10));
-    //promises.push(mtcp.readHoldingRegisters(290, 5));
-    //promises.push(mtcp.readHoldingRegisters(290, 30, {unsigned: true}));
-    //promises.push(mtcp.readInputgRegisters(0, 15));
-    //Promise.all(promises).then(function(results){
-    //    results.forEach(function(result){
-    //        console.log(result);
-    //    });
-    //    exit();
-    //}).catch(function(err){
-    //    console.log(err);
-    //    exit();
-    //});
-    mtcp.writeSingleRegister(290, 333).then(function(res){
-        console.log(res);
+    var promises = [];
+    promises.push(mtcp.readCoils(0, 20));
+    promises.push(mtcp.readDiscreteInputs(0, 10));
+    promises.push(mtcp.readHoldingRegisters(290, 7));
+    promises.push(mtcp.readHoldingRegisters(290, 2, {unsigned: true}));
+    promises.push(mtcp.readInputgRegisters(0, 10));
+    Promise.all(promises).then(function(results){
+        results.forEach(function(result){
+            console.log(result.response);
+        });
         exit();
     }).catch(function(err){
         console.log(err);
         exit();
-    })
+    });
+    //mtcp.writeMultipleCoilsSameValue(21, 1700, false).then(function(res){
+    //    console.log(res);
+    //    exit();
+    //}).catch(function(err){
+    //    console.log(err);
+    //    exit();
+    //})
 }).catch(function(err){
     console.log(err);
     exit();
