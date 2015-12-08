8409be2ed5e90aa0f445f5ea6af9aabe9dabcfe5
Jakub Razak <methodcz@gmail.com>
2015-12-08 17:01:43 +0800

    - Added automatic reconnect in case connection to modbus server is lost - Added reconnect event fired when reconnected - Disconnect is emitted  only once when changing state from connected to disconnect. Error is emitted everytime when connection fails (even for reconnect) - Added polling example

---
+++ examples/polling.js
@@ -0,0 +1,40 @@
+var ModbusTcpClient = require('../index').ModbusTcpClient;
+
+var mtcp = new ModbusTcpClient('localhost', 502, {autoReconnect: true});
+
+mtcp.on('error', function(err){
+    console.log(err);
+});
+
+mtcp.on('reconnect', function(){
+    console.log('RECONNECTED');
+});
+
+mtcp.connect().then(function(){
+    execute(0);
+}).catch(function(err){
+    console.log(err);
+    process.exit();
+});
+
+function execute(r) {
+    if(mtcp.isConnected()) {
+        var r = r + 1;
+        var promises = [];
+        promises.push(mtcp.readCoils(0, 10));
+        promises.push(mtcp.readDiscreteInputs(0, 10));
+        promises.push(mtcp.readHoldingRegisters(290, 10));
+        promises.push(mtcp.readHoldingRegisters(290, 10, {unsigned: true}));
+        promises.push(mtcp.readInputgRegisters(0, 10));
+        Promise.all(promises).then(function(results){
+            console.log("REQUEST "+r);
+            results.forEach(function(result){
+                console.log(JSON.stringify(result.result));
+            });
+        }).catch(function(err){
+            console.log("REQUEST "+r);
+            console.log(err);
+        });
+    }
+    setTimeout(function(){execute(r)}, 500);
+}
